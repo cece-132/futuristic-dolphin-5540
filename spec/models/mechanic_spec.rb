eefe3b93486e062245d8434f42acd7af1b471af8
Candace Eckels <candace.eckels@gmail.com>
2022-09-12 09:17:45 -0600

    Mechanic model created

---
+++ spec/models/mechanic_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Mechanic, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :name}
+    it { should validate_presence_of :years_experience}
+  end
+
+  # describe 'relationships' do
+  #   it { should have_many(:maintenance) }
+  #   it { should have_many(:rides).through(:maintenance) }
+  # end
+end
